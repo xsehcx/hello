8b7786f93bf188b1279874aac2beb638ccf9b66e
xsehcx <xsehcx@gmail.com>
2014-02-22 18:15:06 -0500

    second commit

---
+++ Hello/src/com/canine/HelloD.java
@@ -0,0 +1,11 @@
+package com.canine;
+
+public class HelloD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing some more stuff");
+
+	}
+
+}
